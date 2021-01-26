16591212b1c8b20155b3a4d63f26db0aeaa303b1
Alexander Johr <alexander.johr@gmail.com>
2021-01-26 20:19:49 +0100

    Super Fancy Player Controller Prefab erstellt

---
+++ Assets/Prefabs/Super Fancy Player Controller/PlayFancySound.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayFancySound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource fancySound;
+
+    void Awake()
+    {
+        fancySound = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var leertasteWurdeGedrueckt = Input.GetKeyDown(KeyCode.Space);
+
+        if (leertasteWurdeGedrueckt) {
+            fancySound.Play();
+        }
+    }
+}
