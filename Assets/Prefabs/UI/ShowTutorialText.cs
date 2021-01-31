6887d65f9f1707e56d72b25205ad9757b06b0e9b
Lisa <liza22rf@gmail.com>
2021-01-31 10:35:57 +0100

    add tutorial prefab

---
+++ Assets/Prefabs/UI/ShowTutorialText.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ShowTutorialText : MonoBehaviour
+{
+   [SerializeField]
+   private Transform playerPos;
+   
+   [SerializeField]
+   private string text;
+
+   [SerializeField]
+   private Text textField;
+
+   private void Start()
+   {
+      StartCoroutine(WaitAndPrint());
+   }
+
+   IEnumerator WaitAndPrint()
+   {
+      yield return new WaitUntil(() => playerPos.position.x >= this.gameObject.transform.position.x);
+      textField.text = text;
+      yield return new WaitUntil(() => playerPos.position.x - 0.5 > this.gameObject.transform.position.x);
+   }
+}
